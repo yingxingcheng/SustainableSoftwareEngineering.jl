763a19bd3afbe380c5150dc3f81e923a0e66c2b6
YingXing <yingxing.cheng@mathematik.uni-stuttgart.de>
2023-12-11 12:52:36 +0100

    fix deployments

--- docs/make.jl
+++ docs/make.jl
@@ -15,8 +15,7 @@ makedocs(
           "lecture",
           filter(name->contains(name, ".md"), readdir("docs/src/lecture/"))
         )...
-    ],
-  format = Documenter.HTML(repolink = "https://github.com/yingxingcheng/SustainableSoftwareEngineering.jl.git"),
+    ]
 )
 
 # optionally deploy docs to github pages
